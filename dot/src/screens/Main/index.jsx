22075ccaf35bcb8dd56ee0885a69292f06dfd0e9
downpour07 <choiheeyoung10@gmail.com>
2025-07-15 11:18:33 +0900

    main page

---
+++ dot/src/screens/Main/index.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { View, TouchableWithoutFeedback, StyleSheet, Keyboard } from 'react-native';
+import { color } from '../../styles/theme';
+import constants from '../../styles/constants';
+
+const MainPage = ({navigation}) => {
+    return (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={Styles.container}></View>
+        </TouchableWithoutFeedback>
+    );
+}
+
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: constants.width,
+        height: constants.height,
+        backgroundColor: color.White,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
+
+export default MainPage;
